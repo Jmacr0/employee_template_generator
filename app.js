12ca698d440ac3249f23c1b2bb50b974d61c6d20
Jmacro <jonmacrohon@gmail.com>
2019-12-10 02:31:22 +1100

    recieve prompts and add all employees to an array (as objects)

--- app.js
+++ app.js
@@ -6,33 +6,89 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
+const employeeArray = [];
+
 function promptQuestions() {
     return inquirer
         .prompt([
             {
                 type: "input",
-                message: "What is your email address?",
-                name: "email"
+                message: "What is your name?",
+                name: "name"
             },
             {
                 type: "input",
                 message: "What is your ID?",
-                name: "ID"
+                name: "id"
             },
             {
-                type: "list",
-                message: "What is your favourite colour?",
-                name: "favouriteColour",
-                choices: [
-                    "red",
-                    "blue",
-                    "yellow"
-                ]
+                type: "input",
+                message: "What is your email address?",
+                name: "email"
             },
         ])
 }
 
-async function generateEmployeeSummaries() {
+async function generateEmployeeSummary() {
     const answers = await promptQuestions();
+    const { name, email, id } = answers;
+    const employeeType = await inquirer.prompt({
+        type: "list",
+        message: "What is your role in the company?",
+        name: "role",
+        choices: [
+            "Manager",
+            "Engineer",
+            "Intern"
+        ]
+    })
+    //i used switch at first but scoping prevented id from being read
+    if (employeeType.role === "Manager") {
+        const officeRes = await inquirer.prompt(
+            {
+                type: "input",
+                message: "What is your office number?",
+                name: "office"
+            })
+        const { office } = officeRes;
+        const manager = new Manager(name, email, id, office);
+        employeeArray.push(manager);
+    } else if (employeeType.role === "Engineer") {
+        const githubRes = await inquirer.prompt(
+            {
+                type: "input",
+                message: "What is your github username?",
+                name: "github"
+            })
+        const { github } = githubRes;
+        const engineer = new Engineer(name, email, id, github);
+        employeeArray.push(engineer);
+    } else if (employeeType.role === "Intern") {
+        const schoolRes = await inquirer.prompt(
+            {
+                type: "input",
+                message: "What is your school name?",
+                name: "school"
+            })
+        const { school } = schoolRes
+        const intern = new Intern(name, email, id, school);
+        employeeArray.push(intern);
+    }
+
+    const addEmployee = await inquirer.prompt({
+        type: "list",
+        message: "ADD MORE EMPLOYEES?",
+        name: "add",
+        choices: [
+            "yes",
+            "no"
+        ]
+    })
+    if (addEmployee.add === "yes") {
+        generateEmployeeSummary();
+    } else {
+        console.log(employeeArray);
+    }
+
 }
-generateEmployeeSummaries();
+generateEmployeeSummary();
